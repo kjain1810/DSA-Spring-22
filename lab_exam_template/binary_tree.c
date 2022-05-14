1648c626115e92945c8a03036a6ea96d4f93c556
Tejas Chaudhari <1f4e7.tejas.chaudhari@research.iiit.ac.in>
2022-05-14 22:29:39 +0530

    Added Binary Tree

---
+++ lab_exam_template/binary_tree.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct BT_Node {
+  int val;
+  struct BT_Node *left, *right;
+} BT_Node;
+
+typedef struct BT {
+  BT_Node *root;
+} BT;
+
+// Initalize a binary tree node and return a pointer to it
+BT_Node *BT_Node_init(int val) {
+  BT_Node *n = malloc(sizeof(BT_Node));
+  n->val = val;
+  n->left = n->right = NULL;
+  return n;
+}
+
+// Create a binary tree and return it
+// arr is n x 3, where for the ith node
+// arr[i][0] is parent, arr[i][1] is left/right, arr[i][2] is value
+BT create_BT(int **arr, int n) {
+  BT b;
+  BT_Node *nodes[n];
+  for (int i = 0; i < n; ++i) {
+    nodes[i] = BT_Node_init(arr[i][2]);
+  }
+  for (int i = 0; i < n; ++i) {
+    if (arr[i][0] == -1) {
+      b.root = nodes[i];
+    } else {
+      if (arr[i][1] == 0) {
+        nodes[arr[i][0]]->left = nodes[i];
+      } else {
+        nodes[arr[i][0]]->right = nodes[i];
+      }
+    }
+  }
+  return b;
+}
+
+// Taking input in specified format and creating a binary tree
+int main() {
+  int n;
+  scanf("%d", &n);
+  int **arr = malloc(n * sizeof(int *));
+  for (int i = 0; i < n; ++i) {
+    arr[i] = malloc(3 * sizeof(int));
+  }
+
+  for (int i = 0; i < n; ++i) {
+    int p, c, val;
+    scanf("%d %d %d", &p, &c, &val);
+    arr[i][0] = p;
+    arr[i][1] = c;
+    arr[i][2] = val;
+  }
+  BT b = create_BT(arr, n);
+}
--- lab_exam_template/linked-list.c
+++ lab_exam_template/linked-list.c
@@ -1,96 +1,100 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct LL_Node {
-	int val;
-	struct LL_Node *next;
+  int val;
+  struct LL_Node *next;
 } LL_Node;
 
 typedef struct LL {
-	int size;
-	LL_Node *head;
+  int size;
+  LL_Node *head;
 } LL;
 
 // Initalize and return a pointer to a linked list node
 LL_Node *LL_Node_init(int val) {
-LL_Node *n = malloc(sizeof(LL_Node));
-	n->val = val;
-	n->next = NULL;
-	return n;
+  LL_Node *n = malloc(sizeof(LL_Node));
+  n->val = val;
+  n->next = NULL;
+  return n;
 }
 
 // Initalize and return a pointer to a linked list
 LL *LL_init() {
-	LL *l = malloc(sizeof(LL));
-	l->head = NULL;
-	l->size = 0;
-	return l;
+  LL *l = malloc(sizeof(LL));
+  l->head = NULL;
+  l->size = 0;
+  return l;
 }
 
 // Returns size of a linked list
-int LL_Size(LL *l) {
-	return l->size;
-}
+int LL_Size(LL *l) { return l->size; }
 
-// Add a value to a linked list
-void LL_add(LL *l, int val) {
-	LL_Node *n = LL_Node_init(val);
-	if (l->head == NULL) {
-		l->head = n;
-	} else {
-		LL_Node *curr = l->head;
-		while (curr->next != NULL) {
-			curr = curr->next;
-		}
-		curr->next = n;
-	}
-	l->size += 1;
+// Add a value to linked list position p (0 index)
+// This means that the node at position p will have value = val
+// after this operation
+void LL_add(LL *l, int p, int val) {
+  LL_Node *n = LL_Node_init(val);
+  LL_Node *prev = NULL;
+  LL_Node *curr = l->head;
+  int i = 0;
+  while (i != p) {
+    prev = curr;
+    curr = curr->next;
+    ++i;
+  }
+  if (prev != NULL) {
+    prev->next = n;
+  }
+  n->next = curr;
+  l->size += 1;
 }
 
 // Search for a value in a linked list
 // Returns 1 if found, 0 if not found
 int LL_search(LL *l, int val) {
-	LL_Node *curr = l->head;
-	while (curr != NULL) {
-		if (curr->val == val) {
-			return 1;
-		}
-		curr = curr->next;
-	}
-	return 0;
+  LL_Node *curr = l->head;
+  while (curr != NULL) {
+    if (curr->val == val) {
+      return 1;
+    }
+    curr = curr->next;
+  }
+  return 0;
 }
 
-// Deletes the first value from the linked list, 1 for successful deletion, 0 for no deletion
+// Deletes the first value = val from the linked list, 1 for successful
+// deletion, 0 for no deletion
 int LL_delete(LL *l, int val) {
-	LL_Node *curr = l->head;
-	LL_Node *prev = NULL;
-	while (curr != NULL) {
-		if (curr -> val == val) {
-			if (prev != NULL) {
-				prev->next = curr->next;
-			} else {
-				if (curr->next == NULL) {
-					l->head = NULL;
-				} else {
-					l->head = curr->next;
-				}
-			}
-			free(curr);
-			l->size -= 1;
-			return 1;
-		}
-		prev = curr;
-		curr = curr->next;
-	}
-	return 0;
+  LL_Node *curr = l->head;
+  LL_Node *prev = NULL;
+  while (curr != NULL) {
+    if (curr->val == val) {
+      if (prev != NULL) {
+        prev->next = curr->next;
+      } else {
+        if (curr->next == NULL) {
+          l->head = NULL;
+        } else {
+          l->head = curr->next;
+        }
+      }
+      free(curr);
+      l->size -= 1;
+      return 1;
+    }
+    prev = curr;
+    curr = curr->next;
+  }
+  return 0;
 }
 
 // Prints a linked list
 void LL_print(LL *l) {
-	LL_Node *curr = l->head;
-	while (curr != NULL) {
-		printf("%d ", curr->val);
-		curr = curr->next;
-	}
-	printf("\n");
+  LL_Node *curr = l->head;
+  while (curr != NULL) {
+    printf("%d ", curr->val);
+    curr = curr->next;
+  }
+  printf("\n");
 }
